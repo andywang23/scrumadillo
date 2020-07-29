496c7535778898449b1d38f2b86481f1eb20f024
Luis-KM-Lo <lo.kwun.man@gmail.com>
2020-07-29 12:54:52 -0700

    some breaking changes, not sure if it is the webpack config

--- client/index.js
+++ client/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import store from './store/store';
 import { Provider } from 'react-redux';
+import styles from './scss/application.scss';
 
 ReactDOM.render(
   <Provider store={store}>
